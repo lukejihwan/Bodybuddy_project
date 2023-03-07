a48a6b11079d3e13dd60ccc37625664511ef799e
snsrrhc3078 <snsrrhc3078@gmail.com>
2023-03-07 15:25:44 +0900

    [feat] FreeBoard 작성을 위한 초기작업
    root context에서 model 검색을 위한 component-autoscan 설정, 문자열
    encoding을 위한 filter 설정,
    boardController 작성, 게시판 디자인(list, registform, detail)

--- BodyBuddy/src/main/java/com/edu/bodybuddy/controller/user/UserController.java
+++ BodyBuddy/src/main/java/com/edu/bodybuddy/controller/user/UserController.java
@@ -1,20 +1,31 @@
 package com.edu.bodybuddy.controller.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UserController {
+	private Logger log = LoggerFactory.getLogger(getClass());
 	
 	@GetMapping("/")
 	public String getUserMain() {
-		return "user/index";
+		return "index";
 	}
 	
 	@GetMapping("/{addresses}")
-	public String getPages(@PathVariable String addresses) {
-		return "user/"+addresses;
+	public String getMainPages(@PathVariable String addresses) {
+		log.info(addresses + "카테고리 메인페이지 호출");
+		return addresses;
 	}
-
+	
+	
+	@GetMapping("/{addresses}/{page}")
+	public String getDetailPages(@PathVariable String addresses, @PathVariable String page) {
+		log.info("카테고리 = "+addresses+ ", 페이지명 = "+ page);
+		return addresses+"/"+page;
+	}
+	
 }
---
+++ BodyBuddy/src/main/java/com/edu/bodybuddy/model/board/FreeBoardService.java
@@ -0,0 +1,30 @@
+package com.edu.bodybuddy.model.board;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FreeBoardService implements BoardService{
+
+	public List selectAll() {
+		return null;
+	}
+
+	public Object select(int idx) {
+		return null;
+	}
+
+	public void insert(Object object) {
+		
+	}
+
+	public void update(Object object) {
+		
+	}
+
+	public void delete(int idx) {
+		
+	}
+
+}
