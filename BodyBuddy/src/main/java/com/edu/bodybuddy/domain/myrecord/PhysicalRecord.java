e1cc3cc731e51c901390439b0f88e5f1d19b760d
lukejihwan <luke97211@gmail.com>
2023-03-10 18:48:01 +0900

    2023.03.10 지환 기록추가 화면 모달 창 추가, DTO생성, config매핑 완료

---
+++ BodyBuddy/src/main/java/com/edu/bodybuddy/domain/myrecord/PhysicalRecord.java
@@ -0,0 +1,15 @@
+package com.edu.bodybuddy.domain.myrecord;
+
+
+import lombok.Data;
+
+@Data
+public class PhysicalRecord {
+	private int physical_idx;
+	private int member_idx; //신체, 운동, 식단 기록을 가져올 member_idx
+	private int weight;
+	private int musclemass;
+	private int bodyFat;
+	private int BMI;
+	private String regdate;
+}
