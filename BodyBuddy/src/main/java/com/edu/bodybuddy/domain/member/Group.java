180aedb5b634565b7227161eb5b40bea4ae05d7a
luke <luke9701@naver.com>
2023-03-12 01:05:52 +0900

    2023.03.11 지환 gitignore파일에 .settings 추가

--- BodyBuddy/src/main/java/com/edu/bodybuddy/domain/member/Group.java
+++
@@ -1,9 +0,0 @@
-package com.edu.bodybuddy.domain.member;
-
-import lombok.Data;
-
-@Data
-public class Group {
-	private int group_idx;
-	private String group_name;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# BodyBuddy
-5조 프로젝트 진행용 repository
