20805ba9d9af2c34bf68efd0e7d023263e22a670
lukejihwan <luke97211@gmail.com>
2023-03-29 12:03:03 +0900

    지환 pull 작업중

--- BodyBuddy/src/main/java/com/edu/bodybuddy/domain/myrecord/GpsData.java
+++ BodyBuddy/src/main/java/com/edu/bodybuddy/domain/myrecord/GpsData.java
@@ -1,11 +1,14 @@
 package com.edu.bodybuddy.domain.myrecord;
 
+import com.edu.bodybuddy.domain.member.Member;
+
 import lombok.Data;
 
 // gps 데이터를 받을 객체
 @Data
 public class GpsData {
 	private int gpsdata_idx;
+	private Member member;
 	private double lati;
 	private double longi;
 	private String regdate;
